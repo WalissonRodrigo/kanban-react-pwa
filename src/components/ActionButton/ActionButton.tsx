4d5d3722c274cd973bd94cbf865054e760414575
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2023-03-07 23:31:40 -0300

    action button to generalize actions

---
+++ src/components/ActionButton/ActionButton.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button, Grid } from '@mui/material';
+
+function ActionButton(props: any) {
+  const { confirm, cancel, textButtonSubmit } = props;
+  return (
+    <Grid container spacing={2} direction="row" justifyContent="flex-end">
+      <Grid item>
+        <Button size="small" color="success" variant="contained" onClick={() => confirm()}>
+          {textButtonSubmit || 'OK'}
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button size="small" color="secondary" variant="contained" onClick={() => cancel()}>
+          Cancel
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ActionButton;
