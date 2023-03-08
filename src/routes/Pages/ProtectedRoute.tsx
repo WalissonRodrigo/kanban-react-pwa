21af5e23ebff0c92db2f1cf2ba37079352d02520
Walisson Rodrigo <walisson.rodrigo@turbi.com.br>
2023-03-07 23:41:29 -0300

    fix auth undefined and disable notifications fakes

--- src/routes/Pages/ProtectedRoute.tsx
+++ src/routes/Pages/ProtectedRoute.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
+import { PropsWithChildren } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import { authAtom } from '../../store/_state/auth';
 
-export const ProtectedRoute = ({ children }) => {
+export const ProtectedRoute = (props: PropsWithChildren) => {
+  const { children } = props;
   const auth = useRecoilValue(authAtom);
-  if (!auth.token) {
+  if (!auth?.token) {
     // user is not authenticated
     return <Navigate to="/login" />;
   }
